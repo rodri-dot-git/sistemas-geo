3bccaae8565452b43abae6f99516ff68044b1d55
Rodrigo de Leon <rodri23091998@gmail.com>
2020-02-11 18:43:07 -0600

    practica 1 - API

---
+++ practica1/script.js
@@ -0,0 +1,18 @@
+function initMap() {
+    // The location of Uluru
+    var uluru = {
+        lat: 26.910278,
+        lng: -101.422222
+    };
+    // The map, centered at Uluru
+    var map = new google.maps.Map(
+        document.getElementById('map'), {
+            zoom: 4,
+            center: uluru
+        });
+    // The marker, positioned at Uluru
+    var marker = new google.maps.Marker({
+        position: uluru,
+        map: map
+    });
+}
