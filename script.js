28c334887b2c0a88af6607d37744e3c7c68a8970
Rodrigo de Leon <rodri23091998@gmail.com>
2020-02-17 16:49:22 -0600

    index menu for selecting pages

--- script.js
+++ script.js
@@ -1,18 +1,5 @@
-function initMap() {
-    // The location of Uluru
-    var uluru = {
-        lat: 26.910278,
-        lng: -101.422222
-    };
-    // The map, centered at Uluru
-    var map = new google.maps.Map(
-        document.getElementById('map'), {
-            zoom: 4,
-            center: uluru
-        });
-    // The marker, positioned at Uluru
-    var marker = new google.maps.Marker({
-        position: uluru,
-        map: map
+$(function () {
+    $("#prac1").click(function (event) {
+        window.location.href= "./practica1/index.html";
     });
-}
+});
