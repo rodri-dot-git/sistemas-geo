31bd1bd28057187b69fc0e295ca65e6a0fd4f7e5
Rodrigo de Leon <rodri23091998@gmail.com>
2020-02-25 18:22:40 -0600

    practica 5

---
+++ practica5/script.js
@@ -0,0 +1,29 @@
+var initMap = () => {
+    var pos = {
+        lat: 21.152639,
+        lng: -101.711598
+    };
+    var map = new google.maps.Map(
+        document.getElementById('map'), {
+            zoom: 4,
+            center: pos
+        });
+    var marker = new google.maps.Marker({
+        position: pos,
+        map: map
+    });
+    var info = google.maps.InfoWindow;
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition((pos) => {
+            position = {
+                lat: pos.coords.latitude,
+                lng: pos.coords.latitude
+            }
+            info.setPosition(position)
+            info.setContent(HTML)
+            info.open(map)
+        });
+    } else {
+        console.error("No permitido")
+    }
+}
