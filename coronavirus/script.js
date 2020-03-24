9186f05933e384d6f21f87cf44d509982647b7b5
18355802 <rodri23091998@gmail.com>
2020-03-23 18:23:24 -0600

    ya jala pls

--- coronavirus/script.js
+++ coronavirus/script.js
@@ -21,11 +21,11 @@ var obtieneDatos = async () => {
             var datos = csvJSON(data)
             datos.forEach(lugar => {
                 let info = `
-            <strong>País o provincia:</strong> ${lugar["Province/State"].length > 0 ? lugar["Province/State"] : lugar["Country/Region"]} <br/>
-            <strong>Casos confirmados: </strong>${lugar.Confirmed} <br/>
-            <strong>Muertes: </strong>${lugar.Deaths} <br/>
-            <strong>Recuperados: </strong>${lugar.Recovered} <br/>
-            `
+                    <strong>País o provincia:</strong> ${lugar["Province/State"].length > 0 ? lugar["Province/State"] : lugar["Country/Region"]} <br/>
+                    <strong>Casos confirmados: </strong>${lugar.Confirmed} <br/>
+                    <strong>Muertes: </strong>${lugar.Deaths} <br/>
+                    <strong>Recuperados: </strong>${lugar.Recovered} <br/>
+                `
                 let infowindow = new google.maps.InfoWindow({
                     content: info
                 })
