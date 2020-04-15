fc04c5c17f71d4475b8834cb2834538ef1af9842
rodri-dot-git <rodri230998@gmail.com>
2020-04-15 15:16:27 -0500

    a

--- practica1/script.js
+++ practica1/script.js
@@ -7,12 +7,12 @@ function initMap() {
     // The map, centered at Uluru
     var map = new google.maps.Map(
         document.getElementById('map'), {
-            zoom: 4,
+            zoom: 14,
             center: uluru
         });
     // The marker, positioned at Uluru
-    var marker = new google.maps.Marker({
-        position: uluru,
-        map: map
-    });
+    // var marker = new google.maps.Marker({
+    //     position: uluru,
+    //     map: map
+    // });
 }
