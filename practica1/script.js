ee8e1eb20494bd0154e9af6ff4e61d5097e92c16
rodri-dot-git <rodri230998@gmail.com>
2020-04-15 15:17:53 -0500

    map

--- practica1/script.js
+++ practica1/script.js
@@ -1,8 +1,8 @@
 function initMap() {
     // The location of Uluru
     var uluru = {
-        lat: 26.910278,
-        lng: -101.422222
+        lat: 25.774,
+        lng: -80.190
     };
     // The map, centered at Uluru
     var map = new google.maps.Map(
@@ -15,4 +15,23 @@ function initMap() {
     //     position: uluru,
     //     map: map
     // });
+
+
+    var redCoords = [
+        {lat: 25.774, lng: -80.190},
+        {lat: 18.466, lng: -66.118},
+        {lat: 32.321, lng: -64.757}
+    ];
+
+      // Construct a draggable red triangle with geodesic set to true.
+    new google.maps.Polygon({
+        map: map,
+        paths: redCoords,
+        strokeColor: '#FF0000',
+        strokeOpacity: 0.8,
+        strokeWeight: 2,
+        fillColor: '#FF0000',
+        fillOpacity: 0.35,
+        draggable: true,
+    });
 }
