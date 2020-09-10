56c2e4219048ac677ede99710b19158cd7036388
rodri-dot-git <rodri230998@gmail.com>
2020-09-10 17:19:53 -0500

    fix

--- semestre7/Actividad1/script.js
+++ semestre7/Actividad1/script.js
@@ -1,8 +1,8 @@
 const mapLeaflet = L.map('mapLeaflet').setView([26.926088, -101.424841], 20);
-const greenIcon = L.icon({
+const customIcon = L.icon({
     iconUrl: 'https://i.pinimg.com/originals/90/a1/11/90a111619c271f5c44aeae01b51e4848.gif',
 });
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(mapLeaflet);
-L.marker([26.926088, -101.424841]).addTo(mapLeaflet)
+L.marker([26.926088, -101.424841], {icon: customIcon}).addTo(mapLeaflet)
