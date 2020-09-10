86b2f001e501ae22206b2f30689091134ce39711
rodri-dot-git <rodri230998@gmail.com>
2020-09-10 15:58:56 -0500

    fix

--- practica7/script.js
+++ practica7/script.js
@@ -21,5 +21,7 @@ function obtieneDatos() {
 }
 
 const mapLeaflet = L.map('mapLeaflet').setView([26.926088, -101.424841], 20);
-
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
 L.marker([26.926088, -101.424841]).addTo(map)
