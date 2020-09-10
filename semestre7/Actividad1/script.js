9be1b18054315cb57efad822b137b4b69f4177e4
rodri-dot-git <rodri230998@gmail.com>
2020-09-10 17:21:51 -0500

    size

--- semestre7/Actividad1/script.js
+++ semestre7/Actividad1/script.js
@@ -1,6 +1,7 @@
 const mapLeaflet = L.map('mapLeaflet').setView([26.926088, -101.424841], 20);
 const customIcon = L.icon({
     iconUrl: 'https://www.interbolivia.com/wp-content/uploads/2018/12/pulse.gif',
+    iconSize: [32, 32],
 });
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
